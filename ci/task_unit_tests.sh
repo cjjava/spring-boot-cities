49ed123f0f4d2abf19e40dd2e561358f3f07ef02
chenjun <85309651@qq.com>
2018-08-03 15:46:50 +0800

    update

--- ci/task_unit_tests.sh
+++ ci/task_unit_tests.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-export ROOT_FOLDER=$( pwd )
-export REPO=cities-repo
-
-SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-[[ -f "${SCRIPTS_DIR}/functions.sh" ]] && source "${SCRIPTS_DIR}/functions.sh" || \
-    echo "No functions.sh found"
-
-cd ${ROOT_FOLDER}/${REPO}
-buildGradle
+cd cities-repo/cities-service
+./gradlew build
