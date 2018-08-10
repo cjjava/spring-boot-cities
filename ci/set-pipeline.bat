e6a1f8d33c948d6627cb65a897ed8f4ae484959c
chenjun <85309651@qq.com>
2018-08-10 16:01:41 +0800

    update

--- ci/set-pipeline.bat
+++ ci/set-pipeline.bat
@@ -13,4 +13,4 @@ set PIPELINE_NAME=cities-service
 
 fly -t "%ALIAS%" sp -p "%PIPELINE_NAME%" -c pipeline.yml -l "credentials.yml"
 fly -t "%ALIAS%" up -p "%PIPELINE_NAME%"
-fly -t "%ALIAS%" trigger-job -j %PIPELINE_NAME%/run-acceptance-tests
+fly -t "%ALIAS%" trigger-job -j %PIPELINE_NAME%/run-acceptance-tests -w
--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -6,4 +6,4 @@ PIPELINE_NAME="cities-service"
 
 fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "credentials.yml"
 fly -t "${ALIAS}" up -p "${PIPELINE_NAME}"
-fly -t "${ALIAS}" trigger-job -j ${PIPELINE_NAME}/run-acceptance-tests
+fly -t "${ALIAS}" trigger-job -j ${PIPELINE_NAME}/run-acceptance-tests -w
