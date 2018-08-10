9f2efe3ec912695778787ffa05199090e21c67b6
chenjun <85309651@qq.com>
2018-08-10 15:40:48 +0800

    update

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -5,3 +5,5 @@ ALIAS=${1:-lite}
 PIPELINE_NAME="cities-service"
 
 fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "credentials.yml"
+fly -t "${ALIAS}" up -p "${PIPELINE_NAME}"
+fly -t "${ALIAS}" trigger-job -j ${PIPELINE_NAME}/run-acceptance-tests
